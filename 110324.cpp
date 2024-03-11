32f113d82be6ce74b9862cb35bb4e85bd6fdad7d
Vishal <20363@recbanda.ac.in>
2024-03-11 22:56:21 +0530

    11-03-2024 remove duplicate char in string

--- 110324.cpp
+++ 110324.cpp
@@ -1,1 +1,38 @@
 
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution
+{
+public:
+    string removeDuplicates(string str)
+    {
+        string ans;
+        unordered_map<char, int> map;
+        for (int i = 0; i < str.size(); i++)
+        {
+            map[str[i]]++;
+            if (map[str[i]] == 1)
+            {
+                ans += str[i];
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string str;
+        cin >> str;
+        Solution ob;
+        auto ans = ob.removeDuplicates(str);
+
+        cout << ans << "\n";
+    }
+    return 0;
+}
